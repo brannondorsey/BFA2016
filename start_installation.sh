01900a9f1d01e029188b87108285458267a4dd27
Brannon Dorsey <brannon@brannondorsey.com>
2016-03-05 21:09:29 -0600

    Add start_installation

---
+++ start_installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# SCRIPT_NAME=$(basename $0)
+DIR_NAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+POCSAG_RECORDING_WAV="$DIR_NAME/data/POCSAG_recording.wav"
+POCSAG_RECORDING_CSV="$DIR_NAME/data/POCSAG_recording.csv"
+
+# POCSAG audio recording
+echo "Playing \"$POCSAG_RECORDING_WAV\" in the background."
+sleep 21 && cat $POCSAG_RECORDING_WAV | aplay >> "$DIR_NAME/log/audio.log" 2>&1 &
+AUDIO_PID=$!
+
+# ESCPOS thermal printer
+echo "Launching ESCPOSPrinterDaemon."
+"$DIR_NAME/ESCPOSPrinterDaemon/bin/ESCPOSPrinterDaemon" >> "$DIR_NAME/log/ESCPOSPrinterDaemon.log" 2>&1 &
+
+# POCSAG pager text recording
+echo "Launching holypager reading from file \"$POCSAG_RECORDING_CSV\"."
+"$DIR_NAME/holypager/holypager" "--read=$POCSAG_RECORDING_CSV" --broadcast-freq=163.25e6 --bfa --anonymize --thermal --verbose | tee "$DIR_NAME/log/holypager.log"
