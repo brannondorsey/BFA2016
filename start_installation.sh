161b20a7e1fb865b6c770afaffd161705621b6b6
Brannon Dorsey <brannon@brannondorsey.com>
2016-03-05 23:46:18 -0600

    Add tee to holypager process to generate log

--- start_installation.sh
+++ start_installation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Waiting 10 seconds before executing script..."
+sleep 10
+
 # SCRIPT_NAME=$(basename $0)
 DIR_NAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
@@ -12,9 +15,9 @@ sleep 21 && cat $POCSAG_RECORDING_WAV | aplay >> "$DIR_NAME/log/audio.log" 2>&1 
 AUDIO_PID=$!
 
 # ESCPOS thermal printer
-echo "Launching ESCPOSPrinterDaemon."
-"$DIR_NAME/ESCPOSPrinterDaemon/bin/ESCPOSPrinterDaemon" >> "$DIR_NAME/log/ESCPOSPrinterDaemon.log" 2>&1 &
+echo "Launching ESCPOSPrinterDaemon in the background."
+$DIR_NAME/ESCPOSPrinterDaemon/bin/ESCPOSPrinterDaemon >> "$DIR_NAME/log/ESCPOSPrinterDaemon.log" 2>&1 &
 
 # POCSAG pager text recording
 echo "Launching holypager reading from file \"$POCSAG_RECORDING_CSV\"."
-"$DIR_NAME/holypager/holypager" "--read=$POCSAG_RECORDING_CSV" --broadcast-freq=163.25e6 --bfa --anonymize --thermal --verbose
+/usr/local/bin/node $DIR_NAME/holypager/holypager "--read=$POCSAG_RECORDING_CSV" --broadcast-freq=163.25e6 --bfa --anonymize --thermal --verbose | tee $DIR_NAME/log/holypager.log
